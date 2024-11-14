e656c513cecf7d657fce5e1720e4a6daf4aed143
LinuxSBC <benedict.p.simmons@gmail.com>
2024-11-13 21:46:39 -0600

    Finish time travel assignment

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -27,17 +27,34 @@ updateWithTimeTravel rawGame computer currentModel =
       , history = currentModel.history ++ [computer]
       , historyPlaybackPosition = currentModel.historyPlaybackPosition + 1
       }
+    newPlaybackPosition = 
+      min
+        (mousePosToHistoryIndex computer)
+        (List.length currentModel.history)
+    replayHistory pastInputs = 
+      List.foldl 
+        (\input model -> rawGame.updateState input model)
+        rawGame.initialState
+        pastInputs
   in
     if currentModel.paused && computer.mouse.down then
-      let
-          newPlaybackPosition = 
-            min (mousePosToHistoryIndex computer) (List.length currentModel.history)
-      in
-        { currentModel | historyPlaybackPosition = newPlaybackPosition }
+      { currentModel
+      | historyPlaybackPosition = newPlaybackPosition
+      , rawModel = replayHistory (List.take newPlaybackPosition currentModel.history)
+      }
     else if keyPressed "R" computer then
-      { updatedModel | paused = False }
+      { updatedModel
+      | paused = False
+      , history = List.take currentModel.historyPlaybackPosition currentModel.history
+      }
     else if keyPressed "T" computer then
       { currentModel | paused = True }
+    else if keyPressed "C" computer then
+      { currentModel
+      | rawModel = rawGame.initialState
+      , history = []
+      , historyPlaybackPosition = 0
+      }
     else if currentModel.paused then
       currentModel
     else
@@ -46,10 +63,15 @@ updateWithTimeTravel rawGame computer currentModel =
 viewWithTimeTravel rawGame computer model =
   let
     helpMessage =
-        if model.paused then
-          "Press R to resume"
-        else
-          "Press T to time travel"
+      if model.paused then
+        [ words white "Press R to resume or C to reset"
+          |> move 0 (computer.screen.top - controlBarHeight / 2 + 10)
+        , words white "Drag the bar to move back and forth in time"
+          |> move 0 (computer.screen.top - controlBarHeight / 2 - 10)
+        ]
+      else
+        [ words white "Press T to time travel or C to reset"
+          |> move 0 (computer.screen.top - controlBarHeight / 2)]
     -- Creates a rectangle at the top of the screen, stretching from the
     -- left edge up to a specific position within the history timeline
     historyBar color opacity index =
@@ -65,9 +87,7 @@ viewWithTimeTravel rawGame computer model =
       [ historyBar black 0.3 maxVisibleHistory
       , historyBar (rgb 129 61 156) 0.6 (List.length model.history)
       , historyBar green 0.6 model.historyPlaybackPosition
-      , words white helpMessage
-          |> move 0 (computer.screen.top - controlBarHeight / 2)
-      ]
+      ] ++ helpMessage
 
 keyPressed keyName computer =
   [ String.toLower keyName
